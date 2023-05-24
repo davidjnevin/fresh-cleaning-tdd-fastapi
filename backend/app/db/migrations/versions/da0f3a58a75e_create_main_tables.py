f44759036bbd4c5efc7e4038ce4544f38da22b68
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-24 12:24:24 +0200

    Repository factory

--- backend/app/db/migrations/versions/da0f3a58a75e_create_main_tables.py
+++ backend/app/db/migrations/versions/da0f3a58a75e_create_main_tables.py
@@ -7,7 +7,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic
-revision = 'da0f3a58a75e'
+revision = "da0f3a58a75e"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -19,12 +19,16 @@ def create_cleanings_table() -> None:
         sa.Column("id", sa.Integer, primary_key=True),
         sa.Column("name", sa.Text, nullable=False, index=True),
         sa.Column("description", sa.Text, nullable=True),
-        sa.Column("cleaning_type", sa.Text, nullable=False, server_default="spot_clean"),
+        sa.Column(
+            "cleaning_type", sa.Text, nullable=False, server_default="spot_clean"
+        ),
         sa.Column("price", sa.Numeric(10, 2), nullable=False),
     )
 
+
 def upgrade() -> None:
     create_cleanings_table()
+
 
 def downgrade() -> None:
     op.drop_table("cleanings")

5735f0123a7dac273e3b67eda69f6dc5e0fc4542
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-24 11:24:12 +0200

    Alembic migrations on postgres db

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
 from app.api.routes.cleanings import router as cleanings_router
+from fastapi import APIRouter
+
 router = APIRouter()
 router.include_router(cleanings_router, prefix="/cleanings", tags=["cleanings"])

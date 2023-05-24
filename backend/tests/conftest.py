9bc9067600241bff7a648329384b01bd8c1f7b45
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-24 23:53:32 +0200

    Add update and delete cleaning endpoints

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,14 +1,18 @@
 import warnings
 import os
+
 import pytest
 from asgi_lifespan import LifespanManager
+
 from fastapi import FastAPI
 from httpx import AsyncClient
 from databases import Database
-import alembic
-from alembic.config import Config
+
 from app.models.cleaning import CleaningCreate, CleaningInDB
 from app.db.repositories.cleanings import CleaningsRepository
+
+import alembic
+from alembic.config import Config
 
 
 # Apply migrations at beginning and end of testing session
@@ -17,6 +21,7 @@ def apply_migrations():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     os.environ["TESTING"] = "1"
     config = Config("alembic.ini")
+
     alembic.command.upgrade(config, "head")
     yield
     alembic.command.downgrade(config, "base")
@@ -36,25 +41,22 @@ def db(app: FastAPI) -> Database:
     return app.state._db
 
 
+@pytest.fixture
+async def test_cleaning(db: Database) -> CleaningInDB:
+    cleaning_repo = CleaningsRepository(db)
+    new_cleaning = CleaningCreate(
+        name="fake cleaning name", description="fake cleaning description", price=9.99, cleaning_type="spot_clean",
+    )
+
+    return await cleaning_repo.create_cleaning(new_cleaning=new_cleaning)
+
+
 # Make requests in our tests
 @pytest.fixture
 async def client(app: FastAPI) -> AsyncClient:
     async with LifespanManager(app):
         async with AsyncClient(
-            app=app,
-            base_url="http://testserver",
-            headers={"Content-Type": "application/json"},
+            app=app, base_url="http://testserver", headers={"Content-Type": "application/json"}
         ) as client:
             yield client
 
-
-@pytest.fixture
-async def test_cleaning(db: Database) -> CleaningInDB:
-    cleaning_repo = CleaningsRepository(db)
-    new_cleaning = CleaningCreate(
-        name="fake cleaning name",
-        description="fake cleaning description",
-        price=9.99,
-        cleaning_type="spot_clean",
-    )
-    return await cleaning_repo.create_cleaning(new_cleaning=new_cleaning)

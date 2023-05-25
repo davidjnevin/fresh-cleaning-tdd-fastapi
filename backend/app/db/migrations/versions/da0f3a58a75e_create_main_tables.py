84aaf01440cb18d21a8805281c63bcd730cb99b8
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-25 10:36:59 +0200

    Add user registration endpoints

--- backend/app/db/migrations/versions/da0f3a58a75e_create_main_tables.py
+++ backend/app/db/migrations/versions/da0f3a58a75e_create_main_tables.py
@@ -3,6 +3,7 @@ Revision ID: da0f3a58a75e
 Revises:
 Create Date: 2023-05-24 09:11:58.007599
 """
+from typing import Tuple
 from alembic import op
 import sqlalchemy as sa
 
@@ -11,6 +12,40 @@ revision = "da0f3a58a75e"
 down_revision = None
 branch_labels = None
 depends_on = None
+
+
+def create_updated_at_trigger() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION update_updated_at_column()
+            RETURNS TRIGGER AS
+        $$
+        BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
+        """
+    )
+
+
+def timestamps(indexed: bool = False) -> Tuple[sa.Column, sa.Column]:
+    return (
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed,
+        ),
+    )
 
 
 def create_cleanings_table() -> None:
@@ -23,12 +58,50 @@ def create_cleanings_table() -> None:
             "cleaning_type", sa.Text, nullable=False, server_default="spot_clean"
         ),
         sa.Column("price", sa.Numeric(10, 2), nullable=False),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_cleanings_modtime
+            BEFORE UPDATE
+            ON cleanings
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
+    )
+
+
+def create_users_table() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email_verified", sa.Boolean, nullable=False, server_default="False"),
+        sa.Column("salt", sa.Text, nullable=False),
+        sa.Column("password", sa.Text, nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False, server_default="True"),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False, server_default="False"),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_user_modtime
+            BEFORE UPDATE
+            ON users
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
     )
 
 
 def upgrade() -> None:
+    create_updated_at_trigger()
     create_cleanings_table()
+    create_users_table()
 
 
 def downgrade() -> None:
+    op.drop_table("users")
     op.drop_table("cleanings")
+    op.execute("DROP FUNCTION update_updated_at_column")
